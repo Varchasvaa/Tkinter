71ec61c1d12e1ddf65f9977953b06daa1fe71021
Varchasva <155552006+Varchasvaa@users.noreply.github.com>
2024-03-28 21:39:56 +0530

    Create Memory test.py
    Memory game for fun using tkinter (still some work left to do)

---
+++ Memory test.py
@@ -0,0 +1,56 @@
+from tkinter import * 
+from random import *
+
+win=Tk()
+win.geometry('600x600')
+win.title("Memory")
+
+label=Label(win,text="Testing",font=("timesnewroman",20))
+label.place(x=250,y=250)
+lives=Label(win,text="lives:",font=("timesnewroman",15))
+lives.place(x=0,y=10)
+value=Label(win,text="3",font=("timesnewroman",15))
+value.place(x=52,y=10)
+
+dt=dict()
+word=['Testing','deserve','wing', 'shock', 'letter', 'left', 'hose', 'fireman', 'deeply', 'empty', 'sneaky', 'stew']
+d=dt.fromkeys(word,0)
+
+def end():
+    label.place(x=0,y=300)
+    new.place(x=-100,y=0)
+    seen.place(x=-100,y=0)
+    label.configure(text="Ended",font=("timesnewroman",120))
+
+def newb():
+    words=choice(word)
+    if d[label.cget('text')]==0:
+        d[label.cget('text')]=int(d[label.cget('text')])+1
+        label.configure(text=words)
+    else:
+        value.configure(text=str(int(value.cget('text'))-1))
+        label.configure(text=words)
+    print(d)
+    if int(value.cget('text'))==0:
+        new.after(100,lambda:end())
+
+def seenb():
+    words=choice(word)
+    if d[label.cget('text')]>0:
+        label.configure(text=words)
+    else:
+        value.configure(text=str(int(value.cget('text'))-1))
+        label.configure(text=words)
+    print(d)
+    if int(value.cget('text'))==0:
+        new.after(100,lambda:end())
+
+
+
+new=Button(win,text="New",bd=10,font=("timesnewroman",10),fg='purple',bg="cyan",command=newb)
+new.place(x=220,y=300)
+seen=Button(win,text="Seen",font=("timesnewroman",10),bd=10,fg='purple',bg='cyan',command=seenb)
+seen.place(x=320,y=300)
+
+
+win.mainloop()
